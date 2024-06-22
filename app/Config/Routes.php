5ce1fe383bed0436a4ce3e9b9a39ea1a61eb22a5
Khafid224 <abdulhafid1230@gmail.com>
2024-06-22 23:37:40 +0700

    View Navbar

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,12 +2,6 @@
 
 use CodeIgniter\Router\RouteCollection;
 
-/**
- * @var RouteCollection $routes
- */
+
 $routes->get('/', 'Home::index');
-$routes->get('/about', 'page::about');
-$routes->get('/contact', 'page::contact');
-$routes->get('/fags', 'page::fags');
 $routes->setAutoRoute(true);
-$routes->get('/biodata', 'page::biodata');
---
+++ app/Views/pages/about.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dolorem aut dicta ducimus ullam
+                eaque aliquam nihil quidem blanditiis cumque, possimus incidunt sapiente veritatis fugiat, laborum animi harum quo officiis!</p>
+        </div>
+    </div>
+</div>
